1f7706fad04917a220aa9bb8896816e88ef0b3e6
ivan-chekunkov <chekunkov.ivan@yandex.ru>
2022-09-17 23:28:38 +0300

    adv_t11

--- Generation_python_course_for_advanced/topic_11.py
+++ Generation_python_course_for_advanced/topic_11.py
@@ -1,3 +1,83 @@
+def a_minute_of_genetics():
+    DNK_IN_RNA = {
+        'G': 'C',
+        'C': 'G',
+        'T': 'A',
+        'A': 'U',
+    }
+    dnk = input()
+    for i in dnk:
+        print(DNK_IN_RNA[i], end='')
+
+
+def serial_number():
+    words = {}
+    text = input().split()
+    for word in text:
+        words[word] = words.get(word, 0) + 1
+        print(words[word], end=' ')
+
+
+def scrabble_game():
+    table_score = {
+        1: 'AEILNORSTU',
+        2: 'DG',
+        3: 'BCMP',
+        4: 'FHVWY',
+        5: 'K',
+        8: 'JX',
+        10: 'QZ'
+    }
+    score = 0
+    for char in input():
+        for key, v in table_score.items():
+            if char in v:
+                score += key
+                break
+    print(score)
+
+
+def build_query_string(params):
+    mas = list(str(k) + '=' + str(v) for k, v in params.items())
+    mas.sort()
+    result = '&'.join(i for i in mas)
+    return result
+
+
+def merging_dictionaries(values):
+    values = [{'a': 1, 'b': 2}, {'b': 10, 'c': 100},
+              {'a': 1, 'b': 17, 'c': 50}, {'a': 5, 'd': 777}]
+    result = {}
+    for sl in values:
+        for k, v in sl.items():
+            if result.get(k):
+                result[k].add(v)
+            else:
+                result[k] = set()
+                result[k].add(v)
+    return result
+
+
+def dangerous_virus():
+    commands = {
+        'execute': 'X',
+        'read': 'R',
+        'write': 'W',
+    }
+    virus_files = {}
+    for _ in range(int(input())):
+        file, *atr = input().split()
+        virus_files[file] = set(atr)
+    for _ in range(int(input())):
+        command, file = input().split()
+        atr = virus_files[file]
+        command = commands[command]
+        if command in atr:
+            print('OK')
+            continue
+        print('Access denied')
+
+
 def shopping_in_the_online_store():
     shopping = {}
     for _ in range(int(input())):
