1b303da31ffeac2558afa8449c1b9c45f207ecb5
ivan-chekunkov <chekunkov.ivan@yandex.ru>
2022-10-08 08:53:15 +0300

    adv_t15

--- Generation_python_course_for_advanced/topic_15.py
+++ Generation_python_course_for_advanced/topic_15.py
@@ -73,3 +73,17 @@ def func_2():
     ]
     points.sort(key=compar_2)
     print(points)
+
+
+def compar_3(pair):
+    return min(pair)+max(pair)
+
+
+def func_3():
+    numbers = [
+        (10, 10, 10), (30, 45, 56), (81, 80, 39), (1, 2, 3), (12, 45, 67),
+        (-2, -4, 100), (1, 2, 99), (89, 90, 34), (10, 20, 30), (50, 40, 50),
+        (34, 78, 65), (-5, 90, -1)
+    ]
+    numbers.sort(key=compar_3)
+    print(numbers)
