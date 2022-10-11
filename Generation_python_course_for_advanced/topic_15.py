fa61e4900bcafcb523600f766432414318da3f0e
ivan-chekunkov <chekunkov.ivan@yandex.ru>
2022-10-12 00:07:04 +0300

    adv_t15

--- Generation_python_course_for_advanced/topic_15.py
+++ Generation_python_course_for_advanced/topic_15.py
@@ -170,3 +170,22 @@ def interesting_sorting_2():
     mas = [str(i) for i in mas]
     mas.sort(key=f1)
     print(*mas)
+
+
+def higher_order_functions_1():
+
+    def map(function, items):
+        result = []
+        for item in items:
+            result.append(function(item))
+        return result
+
+    def f(x):
+        return round(x, 2)
+
+    numbers = [
+        3.56773, 5.57668, 4.00914, 56.24241, 9.01344, 32.12013, 23.22222,
+        90.09873, 45.45, 314.1528, 2.71828, 1.41546
+    ]
+    numbers = map(f, numbers)
+    print(*numbers, sep='\n')
