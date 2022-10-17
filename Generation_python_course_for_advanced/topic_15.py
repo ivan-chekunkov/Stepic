7d537e4d52aa65385bfd602250580b3418c4a2ec
ivan-chekunkov <chekunkov.ivan@yandex.ru>
2022-10-17 19:55:21 +0300

    adv_t15

--- Generation_python_course_for_advanced/topic_15.py
+++ Generation_python_course_for_advanced/topic_15.py
@@ -292,3 +292,21 @@ def func_apply(func, nums):
     for n in nums:
         result.append(func(n))
     return result
+
+
+def anonymous_functions_1():
+
+    floats = [4.35, 6.09, 3.25, 9.77, 2.16, 8.88,
+              4.59, 34.23, 12.12, 4.67, 2.45, 9.32]
+    words = ['racecar', 'akinremi', 'deed', 'temidayo',
+             'omoseun', 'civic', 'TATTARRATTAT', 'malayalam', 'nun']
+    numbers = [4, 6, 9, 23, 5]
+
+    map_result = list(map(lambda num: round(num**2, 1), floats))
+    filter_result = list(
+        filter(lambda name: name == name[::-1] and len(name) > 4, words))
+    reduce_result = reduce(lambda num1, num2: num1 * num2, numbers)
+
+    print(map_result)
+    print(filter_result)
+    print(reduce_result)
