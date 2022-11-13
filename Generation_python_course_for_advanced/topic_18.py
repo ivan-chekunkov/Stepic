31ee11ee30a18314fa70037025a54f1a7a55f76e
Ivan Chekunkov <chekunkov.ivan@yandex.ru>
2022-11-13 09:54:40 +0300

    adv_t18

--- Generation_python_course_for_advanced/topic_18.py
+++ Generation_python_course_for_advanced/topic_18.py
@@ -70,3 +70,30 @@ def forbidden_words():
         for word in forbidden_words:
             text = re.sub(word, "*" * len(word), text, flags=re.I | re.M)
         print(text)
+
+
+def transliteration():
+    translate_dict = {
+        'а': 'a', 'к': 'k', 'х': 'h', 'б': 'b', 'л': 'l', 'ц': 'c', 'в': 'v',
+        'м': 'm', 'ч': 'ch',
+        'г': 'g', 'н': 'n', 'ш': 'sh', 'д': 'd', 'о': 'o', 'щ': 'shh',
+        'е': 'e', 'п': 'p', 'ъ': '*',
+        'ё': 'jo', 'р': 'r', 'ы': 'y', 'ж': 'zh', 'с': 's', 'ь': "'", 'з': 'z',
+        'т': 't', 'э': 'je',
+        'и': 'i', 'у': 'u', 'ю': 'ju', 'й': 'j', 'ф': 'f', 'я': 'ya',
+        'А': 'A', 'К': 'K', 'Х': 'H', 'Б': 'B', 'Л': 'L', 'Ц': 'C', 'В': 'V',
+        'М': 'M', 'Ч': 'Ch',
+        'Г': 'G', 'Н': 'N', 'Ш': 'Sh', 'Д': 'D', 'О': 'O', 'Щ': 'Shh',
+        'Е': 'E', 'П': 'P', 'Ъ': '*',
+        'Ё': 'Jo', 'Р': 'R', 'Ы': 'Y', 'Ж': 'Zh', 'С': 'S', 'Ь': "'", 'З': 'Z',
+        'Т': 'T', 'Э': 'Je',
+        'И': 'I', 'У': 'U', 'Ю': 'Ju', 'Й': 'J', 'Ф': 'F', 'Я': 'Ya',
+    }
+    with open(FILES_DIR + 'cyrillic.txt', 'r', encoding='UTF-8') as file:
+        text = file.read()
+    result = ''
+    for char in text:
+        result += translate_dict.get(char, char)
+    with open(FILES_DIR + 'transliteration.txt', 'w',
+              encoding='UTF-8') as file:
+        file.write(result)
