89e23d083fe0959816dea4b0d11452eb17b409e2
Ivan Chekunkov <chekunkov.ivan@yandex.ru>
2022-11-27 14:13:18 +0300

    adv_t17

--- Generation_python_course_for_advanced/topic_17.py
+++ Generation_python_course_for_advanced/topic_17.py
@@ -2,3 +2,15 @@ import os
 
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 FILES_DIR = BASE_DIR + '/files_topic_17/'
+
+
+def a_gift_for_the_new_year():
+    with open(FILES_DIR + 'class_scores.txt', 'r') as file:
+        lines = file.readlines()
+    with open(FILES_DIR + 'new_scores.txt', 'w') as file:
+        for line in lines:
+            name, score = line.split()
+            score = int(score) + 5
+            if score > 100:
+                score = 100
+            file.write(f'{name} {score}\n')
