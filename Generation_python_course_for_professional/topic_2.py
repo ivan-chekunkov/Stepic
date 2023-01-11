d78fef0c2b9fc439cfc2688a8ad71b959e636dbb
Ivan Chekunkov <chekunkov.ivan@yandex.ru>
2023-01-11 22:39:56 +0300

    pro_t2

--- Generation_python_course_for_professional/topic_2.py
+++ Generation_python_course_for_professional/topic_2.py
@@ -6,4 +6,8 @@ FILES_DIR = BASE_DIR + '/files_topic_2/'
 
 def hide_card(card_number: str) -> str:
     card_number = card_number.replace(' ', '')
-    return '*'*12+card_number[12:]
+    return '*' * 12 + card_number[12:]
+
+
+def same_parity(numbers: list) -> list:
+    return list(filter(lambda x: (x-numbers[0]) % 2 == 0, numbers))
