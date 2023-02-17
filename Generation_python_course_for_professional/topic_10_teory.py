c3703b89d44a83886d81859dff4a10f820eaa2c8
Ivan Chekunkov <chekunkov.ivan@yandex.ru>
2023-02-17 20:56:28 +0300

    pro_t10

---
+++ Generation_python_course_for_professional/topic_10_teory.py
@@ -0,0 +1,14 @@
+class GenerateInts:
+    def __init__(self, n):
+        self.n = n
+        self.current = 0
+
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        if self.current == self.n:
+            raise StopIteration
+        else:
+            self.current += 1
+            return self.current - 1
