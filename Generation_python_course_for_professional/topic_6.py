68330d9043d747bc573a54287cdf7162dbcd3f61
Ivan Chekunkov <chekunkov.ivan@yandex.ru>
2023-03-07 22:41:34 +0300

    pro_t6

--- Generation_python_course_for_professional/topic_6.py
+++ Generation_python_course_for_professional/topic_6.py
@@ -37,3 +37,29 @@ def func_3():
         if index == len(obj) - 1:
             return
         print()
+
+
+def func_4():
+    User = namedtuple('User', ['name', 'surname', 'email', 'plan'])
+    users = [User('Mary', 'Griffin', 'sonnen@yahoo.com', 'Basic'),
+             User('Brenda', 'Young', 'retoh@outlook.com', 'Silver'),
+             User('Kathleen', 'Lyons', 'balchen@att.net', 'Gold'),
+             User('Pamela', 'Hicks', 'corrada@sbcglobal.net', 'Silver'),
+             User('William', 'Townsend', 'kosact@verizon.net', 'Gold'),
+             User('Clayton', 'Morris', 'berserk@yahoo.com', 'Silver'),
+             User('Dorothy', 'Dennis', 'sequin@live.com', 'Gold'),
+             User('Tyler', 'Walker', 'noahb@comcast.net', 'Basic'),
+             User('Joseph', 'Moore', 'ylchang@sbcglobal.net', 'Silver'),
+             User('Kenneth', 'Richardson', 'tbusch@me.com', 'Bronze'),
+             User('Stephanie', 'Bush', 'neuffer@live.com', 'Gold'),
+             User('Gregory', 'Hughes', 'juliano@att.net', 'Basic'),
+             User('Tracy', 'Wallace', 'sblack@me.com', 'Silver'),
+             User('Russell', 'Smith', 'isaacson@comcast.net', 'Bronze'),
+             User('Megan', 'Patterson', 'hoangle@outlook.com', 'Basic')]
+    STATUS = {'Gold': 1, 'Silver': 2, 'Bronze': 3, 'Basic': 4}
+    sorted_users = sorted(users, key=lambda x: (STATUS[x.plan], x.email))
+    for user in sorted_users:
+        print(f'{user.name} {user.surname}')
+        print(f'  Email: {user.email}')
+        print(f'  Plan: {user.plan}')
+        print()
