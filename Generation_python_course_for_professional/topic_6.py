3e209f628cc052ccb088fa68877fb5751ea8aed2
Ivan Chekunkov <chekunkov.ivan@yandex.ru>
2023-03-10 20:27:40 +0300

    pro_t6

--- Generation_python_course_for_professional/topic_6.py
+++ Generation_python_course_for_professional/topic_6.py
@@ -83,3 +83,20 @@ def who_are_you_i_didnt_call_you():
         meetings.sort(key=lambda x: x[1])
         for meeting in meetings:
             print(meeting.name)
+
+
+def func_5():
+    data = [
+        ('Books', 1343), ('Books', 1166), ('Merch', 616),
+        ('Courses', 966), ('Merch', 1145), ('Courses', 1061),
+        ('Books', 848), ('Courses', 964), ('Tutorials', 832),
+        ('Merch', 642), ('Books', 815), ('Tutorials', 1041),
+        ('Books', 1218), ('Tutorials', 880), ('Books', 1003),
+        ('Merch', 951), ('Books', 920), ('Merch', 729),
+        ('Tutorials', 977), ('Books', 656)
+    ]
+    data_defdict = defaultdict(int)
+    for elem in data:
+        data_defdict[elem[0]] += (elem[1])
+    for elem in sorted(data_defdict):
+        print(f"{elem}: ${data_defdict[elem]}")
