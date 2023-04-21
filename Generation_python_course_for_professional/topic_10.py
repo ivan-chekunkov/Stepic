7274eab516363e2da9f0761256505a0288b79caa
Ivan Chekunkov <chekunkov.ivan@yandex.ru>
2023-04-21 13:15:20 +0300

    pro_t10

--- Generation_python_course_for_professional/topic_10.py
+++ Generation_python_course_for_professional/topic_10.py
@@ -8,3 +8,18 @@ def four_num():
     next(n)
     next(n)
     print(next(n))
+
+
+def last_num():
+    numbers = [
+        100, 70, 34, 45, 30, 83, 12, 83, -28, 49, -8, -2, 6, 62, 64, -22,
+        -19, 61, 13, 5, 80, -17, 7, 3, 21, 73, 88, -11, 16, -22
+    ]
+    n = iter(numbers)
+    a = 0
+    while True:
+        try:
+            a = next(n)
+        except:
+            print(a)
+            break
