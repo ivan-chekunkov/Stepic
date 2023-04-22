68b7fa81ea4ef10dfdda02fd769c9c681aac21ec
Ivan Chekunkov <chekunkov.ivan@yandex.ru>
2023-04-22 13:15:46 +0300

    pro_t10

--- Generation_python_course_for_professional/topic_10.py
+++ Generation_python_course_for_professional/topic_10.py
@@ -23,3 +23,9 @@ def last_num():
         except:
             print(a)
             break
+
+
+def filterfalse(predicate, iterable):
+    if predicate is None:
+        predicate = bool
+    return filter(lambda x: not predicate(x), iterable)
