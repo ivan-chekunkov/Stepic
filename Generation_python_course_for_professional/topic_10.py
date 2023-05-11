8e9c5b4ef288f996ad4fa0ca309d992b1eb1f288
Ivan Chekunkov <chekunkov.ivan@yandex.ru>
2023-05-11 17:01:52 +0300

    pro_t10

--- Generation_python_course_for_professional/topic_10.py
+++ Generation_python_course_for_professional/topic_10.py
@@ -241,3 +241,23 @@ class Alphabet:
     def __next__(self) -> str:
         self.index += 1
         return self.alphabet[self.index % self.lenth]
+
+
+class Xrange:
+    def __init__(self, start, end, step=1) -> None:
+        self.start = start - step
+        self.end = end
+        self.step = step
+
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        self.start += self.step
+        if self.step > 0:
+            if self.start >= self.end:
+                raise StopIteration
+        else:
+            if self.start <= self.end:
+                raise StopIteration
+        return self.start
