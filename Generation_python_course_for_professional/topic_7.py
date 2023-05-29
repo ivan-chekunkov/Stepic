83002564858fa33790945e8cae93843efa511c13
Ivan Chekunkov <chekunkov.ivan@yandex.ru>
2023-05-29 19:45:04 +0300

    pro_t7

--- Generation_python_course_for_professional/topic_7.py
+++ Generation_python_course_for_professional/topic_7.py
@@ -12,3 +12,15 @@ def func_1():
         except:
             total_likes -= 1
     print(total_likes)
+
+
+def func_2():
+    food = ['chocolate', 'chicken', 'corn', 'sandwich',
+            'soup', 'potatoes', 'beef', 'lox', 'lemonade']
+    fifth = []
+    for x in food:
+        try:
+            fifth.append(x[4])
+        except:
+            fifth.append('_')
+    print(fifth)
