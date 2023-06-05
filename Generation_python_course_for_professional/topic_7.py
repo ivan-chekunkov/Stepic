2ef2857866c329ba8198a1d7e3b10215bd00466e
Ivan Chekunkov <chekunkov.ivan@yandex.ru>
2023-06-05 23:13:55 +0300

    pro_t7

--- Generation_python_course_for_professional/topic_7.py
+++ Generation_python_course_for_professional/topic_7.py
@@ -93,3 +93,11 @@ def get_weekday(number):
     if number not in range(1, 8):
         raise ValueError('Аргумент не принадлежит требуемому диапазону')
     return week[number]
+
+
+def get_id(names: list, name: str) -> int:
+    if not isinstance(name, str):
+        raise TypeError('Имя не является строкой')
+    if not name.istitle() or not name.isalpha():
+        raise ValueError('Имя не является корректным')
+    return len(names) + 1
