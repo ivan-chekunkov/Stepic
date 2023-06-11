4583ced0511721c732ff96cb61579dbfdce9bc0d
Ivan Chekunkov <chekunkov.ivan@yandex.ru>
2023-06-11 11:07:27 +0300

    pro_t9

--- Generation_python_course_for_professional/topic_9.py
+++ Generation_python_course_for_professional/topic_9.py
@@ -4,3 +4,7 @@ def convert(number):
 
 def non_negative_even(numbers):
     return all(map(lambda x: x % 2 == 0 and x >= 0, numbers))
+
+
+def is_greater(lists: list[list], number: int) -> bool:
+    return any(map(lambda x: sum(x) > number, lists))
