d129e3fc8b17ac392c94eae2f725865ca878fdd7
Ivan Chekunkov <chekunkov.ivan@yandex.ru>
2023-06-12 09:18:58 +0300

    pro_t9

--- Generation_python_course_for_professional/topic_9.py
+++ Generation_python_course_for_professional/topic_9.py
@@ -8,3 +8,11 @@ def non_negative_even(numbers):
 
 def is_greater(lists: list[list], number: int) -> bool:
     return any(map(lambda x: sum(x) > number, lists))
+
+
+def custom_isinstance(objects, typeinfo):
+    result = 0
+    for object in objects:
+        if isinstance(object, typeinfo):
+            result += 1
+    return result
