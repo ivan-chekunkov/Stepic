d9b3e233db884c3b746233cba0c3c4e91259dd12
Ivan Chekunkov <chekunkov.ivan@yandex.ru>
2023-06-17 18:58:30 +0300

    pro_t9

--- Generation_python_course_for_professional/topic_9.py
+++ Generation_python_course_for_professional/topic_9.py
@@ -71,3 +71,9 @@ def power(degree):
     def func(x):
         return x**degree
     return func
+
+
+def generator_square_polynom(a, b, c):
+    def func(x):
+        return a * x ** 2 + b * x + c
+    return func
