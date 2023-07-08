a7d3a99e9d63761d0a01157e6d168320c2c88af8
Ivan Chekunkov <chekunkov.ivan@yandex.ru>
2023-07-08 04:13:42 +0300

    oop_t2

--- Generation_python_OOP/topic_2.py
+++ Generation_python_OOP/topic_2.py
@@ -4,3 +4,16 @@ def darts():
         for col in range(1, num + 1):
             print(min(row, col, num - row + 1, num - col + 1), end=' ')
         print()
+
+
+def bracket_sequence():
+    text = input()
+    buffer = 0
+    for char in text:
+        if char == '(':
+            buffer += 1
+        elif char == ')' and buffer > 0:
+            buffer -= 1
+        else:
+            return False
+    return buffer == 0
