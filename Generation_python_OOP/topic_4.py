fd462c7d35de882a40134438a2bb45177884c844
Ivan Chekunkov <chekunkov.ivan@yandex.ru>
2023-07-13 10:15:01 +0300

    oop_t4

---
+++ Generation_python_OOP/topic_4.py
@@ -0,0 +1,16 @@
+class PiggyBank:
+    def __init__(self, balance=0, volume=400):
+        self.balance = balance
+        self.volume = volume
+
+    def add_coins(self, coins):
+        if self.balance + coins > self.volume:
+            raise ValueError('Копилка слишком мала')
+        else:
+            self.balance += coins
+
+    def remove_coins(self, coins):
+        if self.balance - coins < 0:
+            raise ValueError('В копилке недостаточно монет')
+        else:
+            self.balance -= coins
