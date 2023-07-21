53e457908d44efbd1c906291b8cf40f215e47743
Ivan Chekunkov <chekunkov.ivan@yandex.ru>
2023-07-21 09:50:08 +0300

    oop_t4

--- Generation_python_OOP/topic_4.py
+++ Generation_python_OOP/topic_4.py
@@ -98,3 +98,22 @@ class Gun:
 
     def shots_reset(self):
         self.shots = 1
+
+
+class Scales:
+    def __init__(self):
+        self.right_weight = 0
+        self.left_weight = 0
+
+    def add_right(self, weight):
+        self.right_weight += weight
+
+    def add_left(self, weight):
+        self.left_weight += weight
+
+    def get_result(self):
+        if self.left_weight > self.right_weight:
+            return 'Левая чаша тяжелее'
+        elif self.left_weight == self.right_weight:
+            return 'Весы в равновесии'
+        return 'Правая чаша тяжелее'
