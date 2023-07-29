d433ea19ef66832219d663743494aed297fce1e7
Ivan Chekunkov <chekunkov.ivan@yandex.ru>
2023-07-29 12:45:54 +0300

    oop_t4

--- Generation_python_OOP/topic_4.py
+++ Generation_python_OOP/topic_4.py
@@ -290,3 +290,19 @@ class Knight:
                     symbol = '.'
                 print(symbol, end='')
             print()
+
+
+class Circle:
+    def __init__(self, radius):
+        self._radius = radius
+        self._diameter = radius * 2
+        self._area = pi * radius ** 2
+
+    def get_radius(self):
+        return self._radius
+
+    def get_diameter(self):
+        return self._diameter
+
+    def get_area(self):
+        return self._area
