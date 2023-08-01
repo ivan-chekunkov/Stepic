2f77b7f87790010f9ba40ea0e456279f51590770
Ivan Chekunkov <chekunkov.ivan@yandex.ru>
2023-08-01 18:42:57 +0300

    oop_t4

--- Generation_python_OOP/topic_4.py
+++ Generation_python_OOP/topic_4.py
@@ -350,3 +350,18 @@ class User:
             self._age = age
         else:
             raise ValueError('Некорректный возраст')
+
+
+class Rectangle:
+    def __init__(self, length, width):
+        self.length = length
+        self.width = width
+
+    def get_perimeter(self):
+        return self.length * 2 + self.width * 2
+
+    def get_area(self):
+        return self.length * self.width
+
+    perimeter = property(get_perimeter)
+    area = property(get_area)
