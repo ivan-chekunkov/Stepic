a097d1ae290abea44c6eec857440809b87c0dbe5
Ivan Chekunkov <chekunkov.ivan@yandex.ru>
2023-08-05 14:10:53 +0300

    oop_t4

--- Generation_python_OOP/topic_4.py
+++ Generation_python_OOP/topic_4.py
@@ -411,3 +411,10 @@ class Person:
     @fullname.setter
     def fullname(self, fullname):
         self.name, self.surname = fullname.split()
+
+
+def hash_function(password):
+    hash_value = 0
+    for char, index in zip(password, range(len(password))):
+        hash_value += ord(char) * index
+    return hash_value % 10**9
