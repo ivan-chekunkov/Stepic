372b6508ffe07248f20f998f1bbd1f26604de1e0
Ivan Chekunkov <chekunkov.ivan@yandex.ru>
2023-08-10 15:06:41 +0300

    oop_t4

--- Generation_python_OOP/topic_5.py
+++ Generation_python_OOP/topic_5.py
@@ -12,3 +12,16 @@ class Config:
         self.environment = environment
         self.loglevel = loglevel
         self.version = version
+
+
+class Book:
+    def __init__(self, title, author, year):
+        self.title = title
+        self.author = author
+        self.year = year
+
+    def __str__(self):
+        return f'{self.title} ({self.author}, {self.year})'
+
+    def __repr__(self):
+        return f"Book('{self.title}', '{self.author}', {self.year})"
