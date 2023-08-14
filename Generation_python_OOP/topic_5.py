37c02da4ae09827635c52a1c23fb43c4a248d4bf
Ivan Chekunkov <chekunkov.ivan@yandex.ru>
2023-08-15 00:07:30 +0300

    oop_t4

--- Generation_python_OOP/topic_5.py
+++ Generation_python_OOP/topic_5.py
@@ -61,3 +61,19 @@ def is_fraction(string):
         return False
     regex_obj = re.compile(r'-?\d+/\d*[1-9]\d*')
     return bool(regex_obj.fullmatch(string))
+
+
+class Vector:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+    def __repr__(self):
+        return f'Vector({self.x}, {self.y})'
+
+    def __eq__(self, other):
+        if isinstance(other, Vector):
+            return self.x == other.x and self.y == other.y
+        elif isinstance(other, tuple) and len(other) == 2:
+            return self.x == other[0] and self.y == other[1]
+        return NotImplemented
