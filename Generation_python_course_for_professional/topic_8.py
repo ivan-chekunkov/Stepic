9afd8cc453cbed0d076f18567a670bc1d76617a9
Ivan Chekunkov <chekunkov.ivan@yandex.ru>
2023-09-21 12:13:08 +0300

    pro_t8

--- Generation_python_course_for_professional/topic_8.py
+++ Generation_python_course_for_professional/topic_8.py
@@ -2,3 +2,9 @@ def traffic(n):
     if n > 0:
         print("Не парковаться")
         traffic(n - 1)
+
+
+def func1(end, step=1):
+    if step <= end:
+        print(step)
+        func1(end, step + 1)
